6a983bbe8bdbc4f7bae685632dc907724c2042d0
theju1999 <theju199917@gmail.com>
2022-08-09 14:22:35 +0530

    commit

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
     <!-- logged in user information -->
     <?php  if (isset($_SESSION['username'])) : ?>
     	<b><p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p></b>
-		<b><p>Now u can find the fibonocci series </p></b>
+		<b><p>Now u can find the fibonacci series </p></b>
     	<!-- <p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p> -->
     <?php endif ?>
 	
